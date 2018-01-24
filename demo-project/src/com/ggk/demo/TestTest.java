7e0f5057036c733ae8b698a39544d394cbcd8114
narasimha.varahala <narasimha.varahala@GGKU3SER2.ggktech.local>
2018-01-24 14:54:17 +0530

    Test

--- demo-project/src/com/ggk/demo/TestTest.java
+++ demo-project/src/com/ggk/demo/TestTest.java
@@ -5,7 +5,7 @@ public class TestTest
 	public static void main(String args[])
 	{
 		System.out.println("Hello everyone");
-		
+		System.out.println("Adding new line...");
 		System.out.println("Adding new line.shdadfha..");
 		
 		
