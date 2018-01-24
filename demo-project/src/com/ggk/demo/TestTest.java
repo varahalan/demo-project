a6f6ca712be90db51cd3a8b289ff43b9e18d47ed
narasimha.varahala <narasimha.varahala@GGKU3SER2.ggktech.local>
2018-01-24 10:19:23 +0530

    hi

--- demo-project/src/com/ggk/demo/TestTest.java
+++ demo-project/src/com/ggk/demo/TestTest.java
@@ -4,7 +4,6 @@ public class TestTest
 {
 	public static void main(String args[])
 	{
-		System.out.println("Test Message");
 		System.out.println("Hello everyone");
 		System.out.println("Adding new line...");
 		
